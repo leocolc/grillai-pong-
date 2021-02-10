88171456b394faadf3529cff670335b3c529a75d
leocolc <77388309+leocolc@users.noreply.github.com>
2021-02-10 10:54:59 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-
+let picture = image.create(scene.screenWidth(), scene.screenHeight())
+for (let index = 0; index <= scene.screenWidth(); index++) {
+    if (index % 6 < 4) {
+        picture.setPixel(scene.screenWidth() + 2, index, 0)
+    }
+}
+if (false) {
+    for (let index = 0; index <= scene.screenHeight(); index++) {
+        if (index % 1 == 0) {
+            picture.setPixel(scene.screenWidth() + 3, index, 5)
+        }
+    }
+}
+scene.setBackgroundImage(picture)
+let paddle = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . 1 1 1 1 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+paddle.setPosition(8, 60)
